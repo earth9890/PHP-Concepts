d07e6ee1fb9383829cad53085eda586107a19b01
Harish Dhanraj Sugandhi <68777106+harishsganndhi123@users.noreply.github.com>
2024-04-08 11:18:04 +0530

    Hello World and Comments Done

---
+++ hello1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    echo "Hello Neeraj"; ?> <br/>
+     <?php print "Hello Neeraj";  ?> <br/>
+  <?php
+    // Single Comment
+    #  Single Comment
+    /* 
+    It multiline comment
+    */
+   ?>
+     <?php print "Hello Neeraj";  ?> <br/>
+    
+    
+ 
+
+</body>
+</html>
